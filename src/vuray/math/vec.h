4a4208c8045ea0e042f20b296b0ee4d85c43020c
hm2code <hm2code@gmail.com>
2017-09-02 17:38:19 -0700

    Initial commit

---
+++ src/vuray/math/vec.h
@@ -0,0 +1,18 @@
+//
+// vec.h
+// ~~~~~
+// Vector math.
+//
+// Author: hm2code
+//
+#pragma once
+
+namespace vuray {
+namespace math {
+
+template<typename T>
+struct vec2 {
+};
+
+} // math
+} // vuray
---
+++ src/vuray/math/vec_test.cpp
@@ -0,0 +1,13 @@
+//
+// vec_test.cpp
+// ~~~~~~~~~~~~
+// Unit tests for vec.h.
+//
+// Author: hm2code
+//
+#include "vec.h"
+
+int main() {
+    return 0;
+}
+

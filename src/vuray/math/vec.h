7213e4c25f866615a8657c6c60b576f9faca4a4e
hm2code <hm2code@gmail.com>
2017-09-02 23:33:13 -0700

    Add vec template class (draft)

--- src/vuray/math/vec.h
+++ src/vuray/math/vec.h
@@ -6,12 +6,96 @@
 // Author: hm2code
 //
 #pragma once
+#include <type_traits>
 
 namespace vuray {
 namespace math {
 
-template<typename T>
-struct vec2 {
+// Vector element indeces.
+enum { X = 0, Y, Z, W };
+
+// Color element indeces.
+enum { R = 0, G, B, A };
+
+// Vector data storage with a pointer-like interface.
+template<typename T, int SIZE>
+class vec_data {
+public:
+    operator const T*() const { return data; }
+    operator T*() { return data; }
+
+private:
+    T data[SIZE];
+};
+
+// A (very) generic vector.
+template<typename T, int SIZE, typename DATA>
+struct vec {
+public:
+    T x() const { return data[X]; }
+    void set_x(T x) { data[X] = x; }
+
+    T y() const {
+        static_assert(SIZE > 1, "Need a 2 element vector or higher");
+        return data[Y];
+    }
+    void set_y(T y) {
+        static_assert(SIZE > 1, "Need a 2 element vector or higher");
+        data[Y] = y;
+    }
+
+    T z() const {
+        static_assert(SIZE > 2, "Need a 3 element vector or higher");
+        return data[Z];
+    }
+    void set_z(T z) {
+        static_assert(SIZE > 2, "Need a 3 element vector or higher");
+        data[Z] = z;
+    }
+
+    T w() const {
+        static_assert(SIZE > 3, "Need a 4 element vector or higher");
+        return data[W];
+    }
+    void set_w(T w) {
+        static_assert(SIZE > 3, "Need a 4 element vector or higher");
+        data[W] = w;
+    }
+
+    template<typename TT, typename D>
+    vec<T, SIZE, DATA>& operator +=(const vec<TT, SIZE, D> v) {
+        for (int i = 0; i < SIZE; ++i) {
+            data[i] += v.data[i];
+        }
+        return *this;
+    }
+
+    template<typename TT, typename D>
+    vec<T, SIZE, DATA>& operator -=(const vec<TT, SIZE, D> v) {
+        for (int i = 0; i < SIZE; ++i) {
+            data[i] -= v.data[i];
+        }
+        return *this;
+    }
+
+    template<typename TT, typename D>
+    vec<T, SIZE, DATA>& operator *=(const vec<TT, SIZE, D> v) {
+        for (int i = 0; i < SIZE; ++i) {
+            data[i] *= v.data[i];
+        }
+        return *this;
+    }
+
+    template<typename TT, typename D>
+    vec<T, SIZE, DATA>& operator /=(const vec<TT, SIZE, D> v) {
+        for (int i = 0; i < SIZE; ++i) {
+            data[i] /= v.data[i];
+        }
+        return *this;
+    }
+
+private:
+    DATA data;
 };
 
 } // math
--- src/vuray/math/vec_test.cpp
+++ src/vuray/math/vec_test.cpp
@@ -5,9 +5,174 @@
 //
 // Author: hm2code
 //
+#include <vuray/util/unit_test.h>
+
 #include "vec.h"
 
+using namespace vuray::math;
+using namespace vuray::util;
+
+namespace {
+
+void test_vec2_data() {
+    vec_data<float, 2> data;
+    data[X] = 1.f;
+    assert_eq(1.f, data[X], "data[X] == 1");
+    data[Y] = 2.f;
+    assert_eq(2.f, data[Y], "data[Y] == 2");
+}
+
+void test_vec3_data() {
+    vec_data<float, 3> data;
+    data[X] = 1.f;
+    assert_eq(1.f, data[X], "data[X] == 1");
+    data[Y] = 2.f;
+    assert_eq(2.f, data[Y], "data[Y] == 2");
+    data[Z] = 3.f;
+    assert_eq(3.f, data[Z], "data[Z] == 3");
+}
+
+void test_vec4_data() {
+    vec_data<float, 4> data;
+    data[X] = 1.f;
+    assert_eq(1.f, data[X], "data[X] == 1");
+    data[Y] = 2.f;
+    assert_eq(2.f, data[Y], "data[Y] == 2");
+    data[Z] = 3.f;
+    assert_eq(3.f, data[Z], "data[Z] == 3");
+    data[W] = 4.f;
+    assert_eq(4.f, data[W], "data[W] == 4");
+}
+
+void test_vec2_basics() {
+    vec<float, 2, vec_data<float, 2>> v;
+
+    v.set_x(1.f);
+    assert_eq(1.f, v.x(), "v.x() == 1");
+    v.set_y(2.f);
+    assert_eq(2.f, v.y(), "v.y() == 2");
+}
+
+void test_vec3_basics() {
+    vec<float, 3, vec_data<float, 3>> v;
+
+    v.set_x(1.f);
+    assert_eq(1.f, v.x(), "v.x() == 1");
+    v.set_y(2.f);
+    assert_eq(2.f, v.y(), "v.y() == 2");
+    v.set_z(3.f);
+    assert_eq(3.f, v.z(), "v.z() == 3");
+}
+
+void test_vec4_basics() {
+    vec<float, 4, vec_data<float, 4>> v;
+
+    v.set_x(1.f);
+    assert_eq(1.f, v.x(), "v.x() == 1");
+    v.set_y(2.f);
+    assert_eq(2.f, v.y(), "v.y() == 2");
+    v.set_z(3.f);
+    assert_eq(3.f, v.z(), "v.z() == 3");
+    v.set_w(4.f);
+    assert_eq(4.f, v.w(), "v.w() == 4");
+}
+
+void test_operator_add() {
+    vec<float, 3, vec_data<float, 3>> lhs;
+
+    lhs.set_x(1);
+    lhs.set_y(2);
+    lhs.set_z(3);
+
+    vec<float, 3, vec_data<float, 3>> rhs;
+
+    rhs.set_x(2);
+    rhs.set_y(3);
+    rhs.set_z(4);
+
+    lhs += rhs;
+
+    assert_eq(3.f, lhs.x(), "lhs.x() == 3");
+    assert_eq(5.f, lhs.y(), "lhs.y() == 5");
+    assert_eq(7.f, lhs.z(), "lhs.z() == 7");
+}
+
+void test_operator_sub() {
+    vec<float, 3, vec_data<float, 3>> lhs;
+
+    lhs.set_x(1);
+    lhs.set_y(2);
+    lhs.set_z(3);
+
+    vec<float, 3, vec_data<float, 3>> rhs;
+
+    rhs.set_x(4);
+    rhs.set_y(3);
+    rhs.set_z(2);
+
+    lhs -= rhs;
+
+    assert_eq(-3.f, lhs.x(), "lhs.x() == -3");
+    assert_eq(-1.f, lhs.y(), "lhs.y() == -1");
+    assert_eq(1.f, lhs.z(), "lhs.z() == 1");
+}
+
+void test_operator_mul() {
+    vec<float, 3, vec_data<float, 3>> lhs;
+
+    lhs.set_x(1);
+    lhs.set_y(2);
+    lhs.set_z(3);
+
+    vec<float, 3, vec_data<float, 3>> rhs;
+
+    rhs.set_x(2);
+    rhs.set_y(3);
+    rhs.set_z(4);
+
+    lhs *= rhs;
+
+    assert_eq(2.f, lhs.x(), "lhs.x() == 2");
+    assert_eq(6.f, lhs.y(), "lhs.y() == 6");
+    assert_eq(12.f, lhs.z(), "lhs.z() == 12");
+}
+
+void test_operator_div() {
+    vec<float, 3, vec_data<float, 3>> lhs;
+
+    lhs.set_x(30);
+    lhs.set_y(20);
+    lhs.set_z(12);
+
+    vec<float, 3, vec_data<float, 3>> rhs;
+
+    rhs.set_x(5);
+    rhs.set_y(4);
+    rhs.set_z(3);
+
+    lhs /= rhs;
+
+    assert_eq(6.f, lhs.x(), "lhs.x() == 6");
+    assert_eq(5.f, lhs.y(), "lhs.y() == 5");
+    assert_eq(4.f, lhs.z(), "lhs.z() == 4");
+}
+
+} // anonymous
+
 int main() {
+    test_vec2_data();
+    test_vec3_data();
+    test_vec4_data();
+
+    test_vec2_basics();
+    test_vec3_basics();
+    test_vec4_basics();
+
+    test_operator_add();
+    test_operator_sub();
+    test_operator_mul();
+    test_operator_div();
+
     return 0;
 }
 

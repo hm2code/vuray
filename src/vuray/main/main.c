3982932ae87323b3c8338f35e63bf280befae87b
hm2code <hm2code@gmail.com>
2017-09-09 23:57:22 -0700

    Utilize vector math

--- src/vuray/main/main.c
+++ src/vuray/main/main.c
@@ -9,7 +9,17 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <vuray/math/vec.h>
 #include <vuray/util/imageio.h>
+
+static struct vec3 color(struct ray r) {
+    const struct vec3 white = { 1.f, 1.f, 1.f };
+    const struct vec3 blue = { 0.5f, 0.7f, 1.f };
+
+    const struct vec3 unit_dir = vec3_normalize(r.direction);
+    const float t = 0.5f * (unit_dir.y + 1.f);
+    return vec3_add(vec3_mul(white, 1.f - t), vec3_mul(blue, t));
+}
 
 int main(int argc, const char* argv[]) {
     const char* out_file_name = 0;
@@ -32,20 +42,31 @@ int main(int argc, const char* argv[]) {
     const int nx = 200;
     const int ny = 100;
 
+    const struct vec3 ll_corner = { -2.f, -1.f, -1.f };
+    const struct vec3 horiz = { 4.f, 0.f, 0.f };
+    const struct vec3 vert = { 0.f, 2.f, 0.f };
+    const struct vec3 origin = { 0.f, 0.f, 0.f };
+
     const size_t size = 3 * nx * ny;
 
     float* frame_buf = malloc(size * sizeof(float));
 
     int pixel = 0;
-    const float b = 0.2f;
     for (int j = ny - 1; j >= 0; --j) {
-        const float g = (float)j / (float)ny;
+        const float v = (float)j / (float)ny;
+        const struct vec3 s_vert = vec3_mul(vert, v);
         for (int i = 0; i < nx; ++i) {
-            const float r = (float)i / (float)nx;
+            const float u = (float)i / (float)nx;
+            const struct vec3 s_horiz = vec3_mul(horiz, u);
 
-            frame_buf[pixel + 0] = r;
-            frame_buf[pixel + 1] = g;
-            frame_buf[pixel + 2] = b;
+            const struct ray r = (struct ray) {
+                .origin = origin,
+                .direction = vec3_add(ll_corner, vec3_add(s_horiz, s_vert))
+            };
+            const struct vec3 col = color(r);
+            frame_buf[pixel + 0] = col.x;
+            frame_buf[pixel + 1] = col.y;
+            frame_buf[pixel + 2] = col.z;
             pixel += 3;
         }
     }
